72454d437cdad0b4424b7ee1d9a27cc7f9aae2a6
YudinAlexandr <83288129+YudinAlexandr@users.noreply.github.com>
2022-05-20 18:49:33 +0500

    Add files via upload
    Добавлены кнопки "назад"
    Накинут деревянный дизайн на страницу с формой изменения
    Дефолтные значения полей

--- CRUD/update_orders.php
+++ CRUD/update_orders.php
@@ -1,169 +1,55 @@
 <?php
 
-    /*
-     * Подключаем файл для получения соединения к базе данных (PhpMyAdmin, MySQL)
-     */
+//Обновление информации о продукте
 
-    require_once 'config/connect.php';
+/*
+ * Подключаем файл для получения соединения к базе данных (PhpMyAdmin, MySQL)
+ */
 
-    /*
-     * Получаем ID продукта из адресной строки - /orders.php?id=1
-     */
+require_once '../config/connect.php';
 
-    $order_id = $_GET['id'];
+/*
+ * Создаем переменные со значениями, которые были получены с $_POST
+ */
 
-    /*
-     * Делаем выборку строки с полученным ID выше
-     */
-
-    // $order = mysqli_query($connect, "SELECT orders.id, products.name, products.description, summ, orders.col, date_order, clients.name, orders.phone_number, employees.name From orders, products, clients, employees WHERE orders.id_product = products.id and orders.id_client = clients.id and orders.id_employee = employees.id WHERE `id` = '$order_id'");
-
-    // $order = mysqli_query($connect, "SELECT * FROM `orders` WHERE `id` = '$order_id'");
-
-    $order = mysqli_query($connect, "SELECT orders.id, products.name as product_name, products.description, summ, orders.col, date_order, clients.name as client_name, orders.phone_number, employees.full_name From orders, products, clients, employees WHERE orders.id_product = products.id and orders.id_client = clients.id and orders.id_employee = employees.id and orders.id = '$order_id'");
-
-    $employee =  mysqli_query($connect, "SELECT id, full_name From employees");
-    $client =  mysqli_query($connect, "SELECT id, name From clients");
-    $products =  mysqli_query($connect, "SELECT id, name From products");
+$id = $_POST['id'];
+$id_product = $_POST['product'];
+$description = $_POST['description'];
+$summ = $_POST['summ'];
+$col = $_POST['col'];
+$date_order = $_POST['date_order'];
+$id_client = $_POST['client'];
+$phone_number = $_POST['phone_number'];
+$id_employee = $_POST['employee'];
 
 
 
-    /*
-     * Преобразовывем полученные данные в нормальный массив
-     * Используя функцию mysqli_fetch_assoc массив будет иметь ключи равные названиям столбцов в таблице
-     */
 
-    $order = mysqli_fetch_assoc($order);
-    $employee = mysqli_fetch_all($employee);
-    $client = mysqli_fetch_all($client);
-    $products = mysqli_fetch_all($products);
+
+/*
+ * Делаем запрос на изменение строки в таблице orders
+ */
+
+mysqli_query($connect, "UPDATE `orders` SET `id_product` = '$id_product', `description` = '$description', `summ` = '$summ', `col` = '$col', `date_order` = '$date_order', `id_client` = '$id_client', `phone_number` = '$phone_number', `id_employee` = '$id_employee' WHERE `orders`.`id` = '$id'");
+
+
+// `orders` (`id`, `id_product`, `description`, `summ`, `col`, `date_order`, `id_client`, `phone_number`, `id_employee`) VALUES (NULL, '$id_product', '$description', '$summ', '$col', '$date_order', '$id_client', '$phone_number', '$id_employee')"
+
+
+/*
+ * Переадресация на страницу с талицей
+ */
+
+header('Location: /orders.php');
+
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Изменение заказа</title>
-</head>
-<body>
-    <h3>Изменение заказа</h3>
-    <form action="vendor/update_orders.php" method="post">
-        <input type="hidden" name="id" value="<?= $order['id'] ?>">
-        <p>Товар</p>
-                <!-- <input class="form-control" type="text" name="title"></p> -->
-                <select name="product" id="country">
-                  <!-- <option value="" selected="selected"></option> -->
-                  <?php
-                     foreach($products as $val){
-                        if ($order['product_name'] == $val[1])
-                        {
-                            echo '<option value='. $val[0] .' ' . selected . ' >'. $val[1] .'</option>';
-
-                        }
-                        else
-                        {
-                            echo '<option value='. $val[0] .' ' . ' >'. $val[1] .'</option>';
-                        }
-                     }
-                   ?>
-
-                </select>
-        <p>Описание</p>
-        <textarea name="description"><?= $order['description'] ?></textarea>
-        <p>Сумма</p>
-        <input type="number" name="summ" value="<?= $order['summ'] ?>">
-        <p>Количество</p>
-        <input type="number" name="col" value="<?= $order['col'] ?>">
-        <p>Дата заказа</p>
-        <input type="date" name="date_order" value="<?= $order['date_order'] ?>">
-        <p>Клиент</p>
-        <!-- <input type="text" name="id_client" value="<?= $order['client_name'] ?>"> -->
-        <select name="client" id="country">
-                  <!-- <option value="" selected="selected"></option> -->
-                  <?php
-                     foreach($client as $val){
-                        if ($order['client_name'] == $val[1])
-                        {
-                            echo '<option value='. $val[0] .' ' . selected . ' >'. $val[1] .'</option>';
-
-                        }
-                        else
-                        {
-                            echo '<option value='. $val[0] .' ' . ' >'. $val[1] .'</option>';
-                        }
-                     }
-                   ?>
-
-                </select>
-        <p>Номер телефона</p>
-        <input type="number" name="phone_number" value="<?= $order['phone_number'] ?>">
-        <p>Сотрудник</p>
-        <!-- <input type="text" name="id_employee" value="<?= $order['name'] ?>"> -->
-        <select name="employee" id="country">
-                  <!-- <option value="" selected="selected"></option> -->
-                  <?php
-                     foreach($employee as $val){
-                        if ($order['full_name'] == $val[1])
-                        {
-                            echo '<option value='. $val[0] .' ' . selected . ' >'. $val[1] .'</option>';
-
-                        }
-                        else
-                        {
-                            echo '<option value='. $val[0] .' ' . ' >'. $val[1] .'</option>';
-                        }
-                     }
-                   ?>
-
-                </select>
-
-       <!--  <p>Сотрудник</p> -->
-        <!-- <select name="country" id="country">
-          <option value="" selected="selected">Please Choose</option>
-          <?php
-             foreach($employee as $key => $val){
-                 // $selected = ($employee['full_name'] == 'CAMEROON') ? 'selected="selected"' : '';
-                  echo '<option value="'. $employee['full_name'] .'" ' . $selected . ' >'. $key .'</option>';
-             }
-           ?>
-        </select> -->
-
-
-      <!--   <?php
-        foreach($employee as $value)
-        {
-        echo $value;
-        }
-        ?> -->
-
-
-
-
-
-
-         <br> <br>
-        <button type="submit">Изменить</button>
-    </form>
-</body>
-</html>
-
-
-
-<!--
-<input type="hidden" name="id" value="<?= $order['id'] ?>">
-        <p>Товар</p>
-        <input type="text" name="id_product" value="<?= $order['product_name'] ?>">
-        <p>Описание</p>
-        <textarea name="description"><?= $order['description'] ?></textarea>
-        <p>Сумма</p>
-        <input type="number" name="summ" value="<?= $order['summ'] ?>">
-        <p>Количество</p>
-        <input type="number" name="col" value="<?= $order['col'] ?>">
-        <p>Дата заказа</p>
-        <input type="date" name="date_order" value="<?= $order['date_order'] ?>">
-        <p>Клиент</p>
-        <input type="text" name="id_client" value="<?= $order['client_name'] ?>">
-        <p>Номер телефона</p>
-        <input type="number" name="phone_number" value="<?= $order['phone_number'] ?>">
-        <p>Сотрудник</p>
-        <input type="text" name="id_employee" value="<?= $order['name'] ?>"> -->
+<!-- <p><?= $id ?> ds</p>
+<p><?= $id_product ?> ds</p>
+<p><?= $description ?> ds</p>
+<p><?= $summ ?> ds</p>
+<p><?= $col ?> ds</p>
+<p><?= $date_order ?> ds</p>
+<p><?= $id_client ?> ds</p>
+<p><?= $phone_number ?> ds</p>
+<p><?= $id_employee ?> ds</p> -->
